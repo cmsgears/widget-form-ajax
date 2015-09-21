636838a783e8c83548eb7229b184d8d50932b278
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2015-09-21 07:11:18 +0530

    Initial commit.

---
+++ AjaxForm.php
@@ -0,0 +1,23 @@
+<?php
+namespace cmsgears\widgets\aform;
+
+use \Yii;
+use yii\helpers\Html;
+
+class AjaxForm extends cmsgears\widgets\form\BaseForm {
+
+	// Variables ---------------------------------------------------
+
+	// Public Variables --------------------
+
+
+	// Constructor and Initialisation ------------------------------
+
+
+	// Instance Methods --------------------------------------------
+
+	// BasicForm
+
+}
+
+?>
---
+++ README.md
@@ -0,0 +1,11 @@
+Project 	- CMSGears (http://www.cmsgears.org)
+
+Widget  	- Ajax Form
+
+License 	- GPLv3 (http://www.gnu.org/licenses/gpl-3.0.html)
+
+Description - The Ajax Form Widget for CMSGears. It provide options to generate ajax forms stored in db.
+
+Highlighted Features
+=========================================
+1. The widget generate HTML for ajax forms stored in db.
