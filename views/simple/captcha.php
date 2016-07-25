eaebe83c28ff63cf0a4349a445e4c830e9e1c3cf
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2016-07-25 08:49:01 +0530

    Applied quick fixes.

--- views/simple/captcha.php
+++ views/simple/captcha.php
@@ -3,7 +3,7 @@ use yii\captcha\Captcha;
 ?>
 
 <div class='frm-field wrap-captcha'>
-	<?= Captcha::widget([ 'name' => 'GenericForm[captcha]', 'captchaAction' =>  '/forms/site/captcha', 'options' => [ 'placeholder' => 'Captcha Key*' ] ] ); ?>
+	<?= Captcha::widget([ 'name' => 'GenericForm[captcha]', 'captchaAction' =>  '/forms/form/captcha', 'options' => [ 'placeholder' => 'Captcha Key*' ] ] ); ?>
 	<span class="warning">Click on the captcha image to get new code.</span>
 	<span class="error" cmt-error="captcha"></span>
 </div>
